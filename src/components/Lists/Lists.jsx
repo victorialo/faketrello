19f1540a6b9962ec51c189659a214fed9c4318fe
Victoria Lo <victorialo@berkeley.edul>
2023-11-17 17:29:08 -0800

    fix some dependency/out of date build issues

--- src/components/Lists/Lists.jsx
+++ src/components/Lists/Lists.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import styled from 'styled-components/macro';
+import styled from 'styled-components';
 import {DragDropContext, Draggable, Droppable} from 'react-beautiful-dnd';
 
 const List = styled.div`
